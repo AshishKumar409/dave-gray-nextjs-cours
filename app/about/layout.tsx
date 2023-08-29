06c42ca1af463e5ee1b37b6c58bc56a4e5f93d4d
unknown <ashishkumarnath9@gmail.com>
2023-08-29 19:45:19 +0530

    Learned about Routing, Loading State, Error Handling

---
+++ app/about/layout.tsx
@@ -0,0 +1,25 @@
+import { Metadata } from 'next'
+import styles from './style.module.css'
+
+
+export const metadata: Metadata = {
+    title: 'About Page',
+    description: 'This is a about page',
+}
+
+export default function AboutLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <>
+            <nav>
+                About Navbar
+            </nav>
+            <main className={styles.main}>
+                {children}
+            </main>
+        </>
+    )
+}
---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function About() {
+    // throw new Error("Not Today!")
+    return (
+        <>
+            <h1>AboutPage</h1>
+            <Link href="/">Link To Home Page</Link>
+        </>
+    )
+}
